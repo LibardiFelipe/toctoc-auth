72bea1a8c9427019afbd4786df5aa0e9fc8492bc
Felipe Libardi Gonçalves <flibardig@gmail.com>
2025-06-19 14:21:05 -0300

    fix: ajuste nas configs do projeto e bump na versão.

--- index.ts
+++
@@ -1,9 +0,0 @@
-export {
-  TocTocAuthProvider,
-  type TocTocAuthProviderConfig,
-} from "./src/providers/toctoc-provider";
-export { useTocTocAuth } from "./src/hooks/use-toctoc";
-export { TocToc } from "./src/components/toctoc";
-export type { TocTocAuthContent } from "./src/types/toctoc-auth-content";
-export type { TocTocResult } from "./src/types/toctoc-result";
-export type { TocTocContext } from "./src/contexts/toctoc-context";
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+export {
+  TocTocAuthProvider,
+  type TocTocAuthProviderConfig,
+} from "./providers/toctoc-provider";
+export { useTocTocAuth } from "./hooks/use-toctoc";
+export { TocToc } from "./components/toctoc";
+export type { TocTocAuthContent } from "./types/toctoc-auth-content";
+export type { TocTocResult } from "./types/toctoc-result";
+export type { TocTocContext } from "./contexts/toctoc-context";
